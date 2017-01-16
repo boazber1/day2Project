7898fa3bf0e5b1034984dca1b111f03586e1a99c
boaz <djalminya@gmail.com>
2017-01-16 15:03:27 +0200

    switch commit

---
+++ switch.js
@@ -0,0 +1,17 @@
+var someVar= Math.floor(Math.random() * 4) + 1;
+
+switch(someVar){
+	case 1:
+		console.log("one");
+		break;
+	case 2:
+		console.log("two");
+		break;
+        case 3:
+		console.log("three");
+		break;
+	default:
+		console.log("not a number between 1 to 3");
+		
+		
+}
